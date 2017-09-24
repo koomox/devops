cb2e3ef3cb57c2eb27e12877e04daeb4a566424d
allen.w <koomox@163.com>
2017-09-25 00:38:49 +0800

    updated

--- storage/linux/scripts/nginx/centos7x_nginx1121.sh
+++ storage/linux/scripts/nginx/centos7x_nginx1121.sh
@@ -31,3 +31,40 @@ make install
 
 echo 'export PATH=$PATH:/usr/local/nginx/sbin' >> /etc/profile
 source /etc/profile
+
+#============== Run Nginx ================
+touch /usr/lib/systemd/system/nginx.service
+cat >>/usr/lib/systemd/system/nginx.service<<EOF
+[Unit]
+Description=nginx - high performance web server 
+Documentation=http://nginx.org/en/docs/
+After=network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=forking
+PIDFile=/var/run/nginx.pid
+ExecStartPre=/usr/local/nginx/sbin/nginx -t -c /etc/nginx/nginx.conf
+ExecStartPost=/bin/sleep 0.1
+ExecStart=/usr/local/nginx/sbin/nginx -c /etc/nginx/nginx.conf
+ExecReload=/usr/local/nginx/sbin/nginx -s reopen
+ExecStop=/usr/local/nginx/sbin/nginx -s stop
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+#=============== Enable Port =====================
+\cp -f /usr/lib/firewalld/services/http.xml /etc/firewalld/services/http.xml
+\cp -f /usr/lib/firewalld/services/https.xml /etc/firewalld/services/https.xml
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
+firewall-cmd --permanent --zone=public --query-service=http
+firewall-cmd --permanent --zone=public --query-service=https
+firewall-cmd --zone=public --list-all
+
+#============= Start Nginx =======================
+systemctl enable nginx
+
+nginx -v
