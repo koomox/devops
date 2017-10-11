83ab3aebfabaa98e1661deadd18da2445daf5d22
allen.w <koomox@163.com>
2017-10-11 19:32:38 +0800

    updated

--- storage/linux/scripts/centos7x_ss.sh
+++ storage/linux/scripts/centos7x_ss.sh
@@ -4,15 +4,13 @@ cd /tmp/ss
 yum -y install gcc gcc-c++ make perl asciidoc xmlto curl
 
 curl -LO http://www.zlib.net/zlib-1.2.11.tar.gz
-curl -LO https://www.openssl.org/source/openssl-1.0.2l.tar.gz
 curl -LO https://ftp.pcre.org/pub/pcre/pcre-8.41.tar.gz
-curl -LO https://github.com/shadowsocks/shadowsocks-libev/archive/v2.5.2.tar.gz
+curl -LO https://github.com/ARMmbed/mbedtls/archive/mbedtls-2.6.0.tar.gz
 curl -LO https://github.com/shadowsocks/shadowsocks-libev/releases/download/v3.1.0/shadowsocks-libev-3.1.0.tar.gz
 
 tar -zxf zlib-1.2.11.tar.gz
-tar -zxf openssl-1.0.2l.tar.gz
 tar -zxf pcre-8.41.tar.gz
-tar -zxf v2.5.2.tar.gz
+tar -zxf mbedtls-2.6.0.tar.gz
 tar -zxf shadowsocks-libev-3.1.0.tar.gz
 
 #========= install zlib =======================
@@ -25,22 +23,6 @@ touch /etc/ld.so.conf.d/zlib.conf
 echo "/usr/local/zlib/lib" > /etc/ld.so.conf.d/zlib.conf
 
 ldconfig -v
-
-cd ..
-#=========== install openssl ================
-yum -y install perl zlib-devel
-cd openssl-1.0.2l
-\rm -rf /usr/local/openssl /usr/bin/openssl /usr/include/openssl
-./config --openssldir=/usr/local/openssl shared zlib
-make && make install
-
-ln -s /usr/local/openssl/bin/openssl /usr/bin/openssl
-ln -s /usr/local/openssl/include/openssl /usr/include/openssl
-
-touch /etc/ld.so.conf.d/openssl.conf
-echo "/usr/local/openssl/lib" > /etc/ld.so.conf.d/openssl.conf
-ldconfig -v
-openssl version -a
 
 cd ..
 #========= install pcre ================
@@ -60,24 +42,48 @@ source /etc/profile
 pcregrep -V
 
 cd ..
+#========= install mbedTLS ================
+cd mbedtls-mbedtls-2.6.0
+nl Makefile | sed -n '/DESTDIR=/p'
+sed -i '/DESTDIR=\/usr\/local/c DESTDIR=\/usr\/local\/mbedtls' Makefile
+nl Makefile | sed -n '/DESTDIR=/p'
+make && make install
 
+touch /etc/ld.so.conf.d/mbedtls.conf
+echo "/usr/local/mbedtls/lib" > /etc/ld.so.conf.d/mbedtls.conf
+
+ldconfig -v
+
+echo 'export PATH=$PATH:/usr/local/mbedtls/bin' >> /etc/profile
+source /etc/profile
+
+cd ..
 #============ install shadowsocks ====================
-yum -y install asciidoc xmlto
-cd shadowsocks-libev-2.5.2
+#yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto c-ares-devel libev-devel libsodium-devel -y
+yum install gcc gettext autoconf libtool automake make asciidoc xmlto c-ares-devel libev-devel libsodium-devel -y
+cd shadowsocks-libev-3.1.0
 \rm -rf /usr/local/shadowsocks-libev
 ./configure --prefix=/usr/local/shadowsocks-libev \
 --with-pcre=/usr/local/pcre \
---with-zlib=/usr/local/zlib \
---with-zlib-include=/usr/local/zlib/include \
---with-zlib-lib=/usr/local/zlib/lib \
---with-crypto-library=openssl \
---with-openssl=/usr/local/openssl \
---with-openssl-include=/usr/local/openssl/include \
---with-openssl-lib=/usr/local/openssl/lib
+--with-mbedtls=/usr/local/mbedtls \
+--with-mbedtls-include=/usr/local/mbedtls/include \
+--with-mbedtls-lib=/usr/local/mbedtls/lib
+
 make && make install
 
-#=========== 
-cd shadowsocks-libev-3.1.0
+mkdir -p /var/run/shadowsocks-libev
+mkdir -p /etc/shadowsocks-libev
+
+touch /etc/ld.so.conf.d/shadowsocks-libev.conf
+echo "/usr/local/shadowsocks-libev/lib" > /etc/ld.so.conf.d/shadowsocks-libev.conf
+
+ldconfig -v
+
+echo 'export PATH=$PATH:/usr/local/shadowsocks-libev/bin' >> /etc/profile
+source /etc/profile
+
+cd ..
+#===========
 
 ldd /usr/local/shadowsocks-libev/bin/ss-server
 
