40368e6b10f37f53c1dde5f9618093d29c0dab94
allen.w <koomox@163.com>
2017-10-11 17:53:05 +0800

    updated

--- storage/linux/scripts/centos7x_linode_iptables.sh
+++ storage/linux/scripts/centos7x_linode_iptables.sh
@@ -71,8 +71,6 @@ iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport ${SSH_PORT} -j ACCE
 iptables -A INPUT -m state --state NEW -m udp -p udp --sport 123 -j ACCEPT
 iptables -A INPUT -p tcp --sport 16630 -j ACCEPT
 
-iptables -A INPUT -i ppp+ -j ACCEPT
-
 # OUTPUT
 iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A OUTPUT -m icmp -p icmp --icmp-type any -j ACCEPT
@@ -85,6 +83,14 @@ iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 16630 -j ACCEPT
+
+### Outlook.com
+iptables -A OUTPUT -p tcp --dport 993 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 587 -j ACCEPT
+
+### gmail
+iptables -A OUTPUT -p tcp --dport 995 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 465 -j ACCEPT
 
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
