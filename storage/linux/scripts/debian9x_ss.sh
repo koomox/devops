e4980d3d20fc3e0966406fadb42b14dcba832898
koomox <koomoxs@gmail.com>
2018-10-29 10:48:06 +0800

    up

--- storage/linux/scripts/debian9x_ss.sh
+++ storage/linux/scripts/debian9x_ss.sh
@@ -9,3 +9,9 @@ ldd -r /usr/bin/ss-server
 systemctl stop shadowsocks-libev
 systemctl disable shadowsocks-libev
 systemctl status shadowsocks-libev
+
+echo '#!/bin/sh -e' > /etc/rc.local
+echo 'iptables-restore < /etc/iptables.rules' >> /etc/rc.local
+echo 'mkdir -p /var/run/shadowsocks-libev/' >> /etc/rc.local
+echo 'exit 0' >> /etc/rc.local
+chmod +x /etc/rc.local
