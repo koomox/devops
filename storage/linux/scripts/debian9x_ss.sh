acdb01976421552a9a22be90f7236b1c069b0b64
koomox <koomoxs@gmail.com>
2018-10-29 09:16:26 +0800

    up

---
+++ storage/linux/scripts/debian9x_ss.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update -y
+apt-get upgrade -y
+sudo sh -c 'printf "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list'
+sudo apt update -y
+sudo apt -t stretch-backports install shadowsocks-libev -y
+ldd -r /usr/bin/ss-server
