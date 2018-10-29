ace373ece7881a71a76f6b4bd7188e294bc214bd
koomox <koomoxs@gmail.com>
2018-10-29 16:19:35 +0800

    up

--- storage/linux/scripts/debian9x_mtproxy.sh
+++ storage/linux/scripts/debian9x_mtproxy.sh
@@ -13,14 +13,15 @@ read MTPROXY_SECRET
 echo "========= MTProxy Secret ============="
 echo "${MTPROXY_SECRET}"
 
-\rm -rf /opt/MTProxy
+\rm -rf /opt/MTProxy /usr/local/MTProxy
+mkdir -p /usr/local/MTProxy
 cd /opt
 apt-get install git curl build-essential libssl-dev zlib1g-dev -y
 git clone https://github.com/TelegramMessenger/MTProxy
 cd MTProxy
 make && cd objs/bin
-mv mtproto-proxy -t /opt/MTProxy
-cd /opt/MTProxy
+mv mtproto-proxy -t /usr/local/MTProxy
+cd /usr/local/MTProxy
 curl -s https://core.telegram.org/getProxySecret -o proxy-secret
 curl -s https://core.telegram.org/getProxyConfig -o proxy-multi.conf
 
@@ -31,8 +32,8 @@ After=network.target
 
 [Service]
 Type=simple
-WorkingDirectory=/opt/MTProxy
-ExecStart=/opt/MTProxy/mtproto-proxy -u nobody -p 8888 -H ${MTPROXY_PORT} -S "${MTPROXY_SECRET}" --aes-pwd proxy-secret proxy-multi.conf -M 1
+WorkingDirectory=/usr/local/MTProxy
+ExecStart=/usr/local/MTProxy/mtproto-proxy -u nobody -p 8888 -H ${MTPROXY_PORT} -S "${MTPROXY_SECRET}" --aes-pwd proxy-secret proxy-multi.conf -M 1
 Restart=on-failure
 
 [Install]
@@ -42,6 +43,8 @@ EOF
 iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport ${MTPROXY_PORT} -j ACCEPT
 iptables -A OUTPUT -p tcp --sport ${MTPROXY_PORT} -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 8888 -j ACCEPT
+
+iptables-save > /etc/iptables.rules
 
 systemctl daemon-reload
 systemctl restart MTProxy.service
