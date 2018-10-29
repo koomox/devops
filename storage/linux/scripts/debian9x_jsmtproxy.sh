d009b134d86f8f7bfffcf1aab7e2622067cebbdf
koomox <koomoxs@gmail.com>
2018-10-29 09:48:25 +0800

    up

---
+++ storage/linux/scripts/debian9x_jsmtproxy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+NODE_VER=v6.14.4
+MTPROXY_PORT=443
+MTPROXY_SECRET="*****"
+echo "========= setting Node.js Version ======"
+echo "please input Node.js Version: "
+read NODE_VER
+echo "${NODE_VER}"
+
+echo "========= setting MTProxy Port And Secret ======"
+echo "please input MTProxy PORT: "
+read MTPROXY_PORT
+echo "========= MTProxy PORT ============="
+echo "${MTPROXY_PORT}"
+
+echo "please input MTProxy Secret: "
+read MTPROXY_SECRET
+echo "========= MTProxy Secret ============="
+echo "${MTPROXY_SECRET}"
+
+cd /tmp
+apt-get install curl git
+curl -LO https://nodejs.org/dist/latest-v6.x/node-${NODE_VER}-linux-x64.tar.xz
+xz -d node-${NODE_VER}-linux-x64.tar.xz
+tar -xf node-${NODE_VER}-linux-x64.tar
+\rm -rf /usr/local/node
+mv node-${NODE_VER}-linux-x64 /usr/local/node
+echo 'export NODE_HOME=/usr/local/node' >> /etc/profile
+echo 'export PATH=$PATH:$NODE_HOME/bin' >> /etc/profile
+echo 'export NODE_PATH=$PATH:$NODE_HOME/lib/node_modules' >> /etc/profile
+source /etc/profile
+
+npm install -g pm2
+cd /opt
+git clone https://github.com/FreedomPrevails/JSMTProxy.git
+
+cat > /opt/JSMTProxy/config.json << EOF
+{
+	"port":${MTPROXY_PORT},
+	"secret":"${MTPROXY_SECRET}"
+}
+EOF
+
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport ${MTPROXY_PORT} -j ACCEPT
+iptables -A OUTPUT -p tcp --sport ${MTPROXY_PORT} -j ACCEPT
+
+cd /opt/JSMTProxy && pm2 start mtproxy.js -i max
