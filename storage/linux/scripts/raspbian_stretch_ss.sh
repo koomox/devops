e16b3298060285752a88fc9084098573d2d2ea27
koomox <koomoxs@gmail.com>
2018-11-29 19:48:11 +0800

    up

--- storage/linux/scripts/raspbian_stretch_ss.sh
+++ storage/linux/scripts/raspbian_stretch_ss.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 SS_LOCAL_CONF=/etc/shadowsocks-libev/ss-local.json
+server_method=aes-256-cfb
+
 sudo sh -c 'printf "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list'
 sudo apt update
 sudo apt -t stretch-backports install shadowsocks-libev
-
-apt install shadowsocks-libev
 
 echo "=============== 初始化 shadowsocks-libev-Local ====================="
 read -p "请输入服务器地址: " server_address
 read -p "请输入服务器端口: " server_port
 read -p "请输入服务器密钥: " server_secret
-read -p "请输入加密协议: " server_method
+read -p "请输入加密协议:(aes-256-cfb) " server_method
 read -p "请输入本地端口: " local_port
 
 mkdir -p /etc/shadowsocks-libev
