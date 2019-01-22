92eae730154c8c1da2c103870f4acaf579c3dc0b
koomox <koomoxs@gmail.com>
2019-01-22 12:12:44 +0800

    up

--- storage/linux/scripts/nodejs/lts_nodejs.sh
+++ storage/linux/scripts/nodejs/lts_nodejs.sh
@@ -9,12 +9,16 @@ check_os_bits() {
 	fi
 }
 check_os_bits
-
-cd /tmp
-\rm -rf /usr/local/node /tmp/node.tar.gz
-mkdir -p /usr/local/node
 NODE_VERSION=$(wget -q -O - https://nodejs.org/en/download/ | grep -E "Latest LTS Version" | sed -E "s/.*<strong>([0-9]+\.[0-9]+\.[0-9]+).*/\1/gm")
 NODE_BITS=${bit}
+
+cd /tmp
+if [ -d /usr/local/node ]; then
+	\rm -rf /usr/local/node
+fi
+mkdir -p /usr/local/node
 wget https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-${NODE_BITS}.tar.xz
 xz -d node-v${NODE_VERSION}-linux-${NODE_BITS}.tar.xz
-tar -xf node-v${NODE_VERSION}-linux-${NODE_BITS}.tar -C /opt/node
+tar -xf node-v${NODE_VERSION}-linux-${NODE_BITS}.tar
+mv node-v${NODE_VERSION}-linux-${NODE_BITS} /usr/local/node
+\rm -rf node-v${NODE_VERSION}-linux-${NODE_BITS}.tar.xz node-v${NODE_VERSION}-linux-${NODE_BITS}.tar
