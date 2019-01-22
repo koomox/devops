be0408c28255083e804b034c28dbbb3fdfaa9489
koomox <koomoxs@gmail.com>
2019-01-22 13:15:53 +0800

    up

--- storage/linux/scripts/os/check_os.sh
+++ storage/linux/scripts/os/check_os.sh
@@ -3,9 +3,9 @@ check_sys(){
 	if grep -Eqi "CentOS|Red Hat|RedHat" /etc/issue || grep -Eq "CentOS|Red Hat|RedHat" /etc/*-release || grep -Eqi "CentOS|Red Hat|RedHat" /proc/version; then
 		release="CentOS"
 	elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
-		release="debian"
-	elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release || grep -Eqi "Fedora" /proc/version; then
 		release="Debian"
+	elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release || grep -Eqi "Fedora" /proc/version; then
+		release="Fedora"
 	elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release || grep -Eqi "Ubuntu" /proc/version; then
 		release="Ubuntu"
 	elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
@@ -15,7 +15,7 @@ check_sys(){
 	else
 		release="unknown"
 	fi
-}
+}ss
 
 check_sys
 echo "Linux 发行版: " $release
