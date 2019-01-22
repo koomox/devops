9902dd5394191bed0d046285ab732bbf063979e2
koomox <koomoxs@gmail.com>
2019-01-22 15:12:48 +0800

    up

--- linux/nodejs.md
+++ linux/nodejs.md
@@ -1,36 +1,20 @@
 # Node.js              
 ### 一键安装 LTS Node.js            
-Linux 一键安装 LTS Node.js，自动检测系统类型，安装最新版 LTS Node.js。          
+Linux 一键安装 Node.js，自动检测系统类型，安装最新稳定版 Node.js。          
 ```sh
 curl -LO https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nodejs/lts_nodejs.sh
 chmod +x ./lts_nodejs.sh
 ./lts_nodejs.sh
 ```
-### 一键安装 LTS Node.js            
-Linux 一键安装 LTS Node.js，自动检测系统类型，安装最新版 LTS Node.js。          
+### 一键安装最新发行版 Node.js            
+Linux 一键安装 Node.js，自动检测系统类型，安装最新发行版 Node.js。          
 ```sh
 curl -LO https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nodejs/lts_nodejs.sh
 chmod +x ./lts_nodejs.sh
 ./lts_nodejs.sh
 ```
-### Debian 9.x 搭建 Node.js 环境            
-Debian 9.x 一键安装最新版 Node.js          
 ```sh
-curl -LO https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nodejs/debian9x_nodejs.sh
-chmod +x ./debian9x_nodejs.sh
-./debian9x_nodejs.sh
-```
-### CentOS 7.x 搭建 Node.js 环境                  
-CentOS 7.x 一键安装最新版 Node.js          
-```sh
-curl -LO https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nodejs/centos7x_nodejs.sh
-chmod +x ./centos7x_nodejs.sh
-./centos7x_nodejs.sh
-```
-### Raspbian 搭建 Node.js 环境                  
-Raspbian 一键安装最新版 Node.js          
-```sh
-curl -LO https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nodejs/raspbian_nodejs.sh
-chmod +x ./raspbian_nodejs.sh
-./raspbian_nodejs.sh
+curl -LO https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nodejs/latest_nodejs_v2.sh
+chmod +x ./latest_nodejs_v2.sh
+./latest_nodejs_v2.sh
 ```
---
+++ storage/linux/scripts/nodejs/latest_nodejs_v2.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+check_sys(){
+	if grep -Eqi "CentOS|Red Hat|RedHat" /etc/issue || grep -Eq "CentOS|Red Hat|RedHat" /etc/*-release || grep -Eqi "CentOS|Red Hat|RedHat" /proc/version; then
+		release="CentOS"
+	elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+		release="Debian"
+	elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release || grep -Eqi "Fedora" /proc/version; then
+		release="Fedora"
+	elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release || grep -Eqi "Ubuntu" /proc/version; then
+		release="Ubuntu"
+	elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+		release="Raspbian"
+	elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+		release="Aliyun"
+	else
+		release="unknown"
+	fi
+
+	if [ ! -f /usr/bin/wget ]; then
+		if [[ ${release} == "CentOS" || ${release} == "Fedora" ]]; then
+			yum install wget -y
+		elif [[ ${release} == "Debian" || ${release} == "Ubuntu" || ${release} == "Raspbian" || ${release} == "Aliyun" ]]; then
+			apt install wget -y
+		fi
+	fi
+}
+
+check_os_bits() {
+	bit=$(uname -m)
+	if [[ ${bit} == "x86_64" ]]; then
+		bit="x64"
+	elif [[ ${bit} == "i386" || ${bit} == "i686" ]]; then
+		bit="x86"
+	fi
+}
+
+node_environmental(){
+	if grep -Eqi "NODE_HOME" /etc/profile; then
+		source /etc/profile
+	else
+		echo 'export NODE_HOME=/usr/local/node' >> /etc/profile
+		echo 'export PATH=$PATH:$NODE_HOME/bin' >> /etc/profile
+		echo 'export NODE_PATH=$PATH:$NODE_HOME/lib/node_modules' >> /etc/profile
+		source /etc/profile
+	fi
+}
+
+check_sys
+check_os_bits
+NODE_VERSION=$(wget -q -O - https://github.com/nodejs/node/tags | grep -m1 -E "/nodejs/node/releases/tag/v[0-9]+\.[0-9]+\.[0-9]+" | sed -E "s/.*v([0-9]+\.[0-9]+\.[0-9]+).*/\1/gm")
+NODE_BITS=${bit}
+
+cd /tmp
+if [ -e /usr/local/node ]; then
+	\rm -rf /usr/local/node
+fi
+if [ -f node-v${NODE_VERSION}-linux-${NODE_BITS}.tar.xz ]; then
+	\rm -rf node-v${NODE_VERSION}-linux-${NODE_BITS}.tar.xz
+fi
+if [ -f node-v${NODE_VERSION}-linux-${NODE_BITS}.tar ]; then
+	\rm -rf node-v${NODE_VERSION}-linux-${NODE_BITS}.tar
+fi
+wget https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-${NODE_BITS}.tar.xz
+xz -d node-v${NODE_VERSION}-linux-${NODE_BITS}.tar.xz
+tar -xf node-v${NODE_VERSION}-linux-${NODE_BITS}.tar
+mv node-v${NODE_VERSION}-linux-${NODE_BITS} /usr/local/node
+\rm -rf node-v${NODE_VERSION}-linux-${NODE_BITS}.tar.xz node-v${NODE_VERSION}-linux-${NODE_BITS}.tar
+
+node_environmental
+echo "install Node.js ${NODE_VERSION} Success!"
+node -v
