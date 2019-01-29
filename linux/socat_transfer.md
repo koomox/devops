78523db18de1d82fa901f5319bd10cecf6716ad6
koomox <koomoxs@gmail.com>
2019-01-29 23:02:34 +0800

    up

--- linux/socat_transfer.md
+++ linux/socat_transfer.md
@@ -5,3 +5,14 @@ socat: [官网](http://www.dest-unreach.org/socat/)
 ```sh
 apt install socat
 ```
+### Debian 9.x 一键安装脚本            
+Debian 9.x 一键安装 socat 脚本。        
+```sh
+wget https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/socat/debian9x_socat.sh -o /tmp/debian9x_socat.sh
+chmod +x /tmp/debian9x_socat.sh
+/tmp/debian9x_socat.sh
+```
+添加到系统自启动             
+```sh
+echo '/etc/socat/run.sh' >> /etc/rc.local
+```
---
+++ storage/linux/scripts/socat/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+socat_add_user() {
+user=$1
+ip=$2
+public_port=$3
+
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport ${public_port} -j ACCEPT
+iptables -A OUTPUT -p tcp --sport ${public_port} -j ACCEPT
+iptables -A OUTPUT -p tcp --dport ${public_port} -j ACCEPT
+
+nohup socat TCP4-LISTEN:${public_port},reuseaddr,fork TCP4:${ip}:${public_port} >> /var/log/socat.log 2>&1 &
+nohup socat UDP4-LISTEN:${public_port},reuseaddr,fork UDP4:${ip}:${public_port} >> /var/log/socat.log 2>&1 &
+}
