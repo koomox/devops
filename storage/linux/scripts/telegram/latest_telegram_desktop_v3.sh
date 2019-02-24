ec0a46b7024ebcffc060d76f548ce023dd673c84
koomox <koomoxs@gmail.com>
2019-02-24 12:29:59 +0800

    up

--- storage/linux/scripts/telegram/latest_telegram_desktop_v3.sh
+++ storage/linux/scripts/telegram/latest_telegram_desktop_v3.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-go_environmental(){
+installation_dependency(){
+	if grep -Eqi "CentOS|Red Hat|RedHat" /etc/issue || grep -Eq "CentOS|Red Hat|RedHat" /etc/*-release || grep -Eqi "CentOS|Red Hat|RedHat" /proc/version; then
+		release="CentOS"
+	elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+		release="Debian"
+	elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release || grep -Eqi "Fedora" /proc/version; then
+		release="Fedora"
+	elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release || grep -Eqi "Ubuntu" /proc/version; then
+		release="Ubuntu"
+	elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+		release="Raspbian"
+	elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+		release="Aliyun"
+	else
+		release="unknown"
+	fi
+
+	if [ ! `command -v wget >/dev/null` ]; then
+		if [[ ${release} == "CentOS" || ${release} == "Fedora" ]]; then
+			yum install wget -y
+		elif [[ ${release} == "Debian" || ${release} == "Ubuntu" || ${release} == "Raspbian" || ${release} == "Aliyun" ]]; then
+			apt install wget -y
+		fi
+	fi
+}
+
+tg_environmental(){
 	if grep -Eqi "/usr/local/telegram-desktop/bin" /etc/profile; then
 		source /etc/profile
 	else
@@ -21,8 +47,18 @@ if [ -e /usr/local/telegram-desktop ]; then
 	\rm -rf /usr/local/telegram-desktop
 fi
 
+if [ -e /tmp/Telegram ]; then
+	\rm -rf /tmp/Telegram
+fi
+
 mkdir -p /usr/local/telegram-desktop/bin
 
 wget https://github.com/telegramdesktop/tdesktop/releases/download/v${TG_VERSION}/tsetup.${TG_VERSION}.tar.xz
 xz -d tsetup.${TG_VERSION}.tar.xz
 tar -xf tsetup.${TG_VERSION}.tar
+cd /tmp/Telegram
+mv Telegram Updater -t /usr/local/telegram-desktop/bin
+
+tg_environmental
+echo "The Telegram-Desktop ${TG_VERSION} install Success!"
+Telegram
