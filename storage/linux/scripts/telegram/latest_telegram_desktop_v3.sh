d4a987053429a4a2e90cfdf65da807fdf558b08a
koomox <koomoxs@gmail.com>
2019-02-25 12:04:09 +0800

    up

--- storage/linux/scripts/telegram/latest_telegram_desktop_v3.sh
+++ storage/linux/scripts/telegram/latest_telegram_desktop_v3.sh
@@ -26,15 +26,6 @@ installation_dependency(){
 	fi
 }
 
-tg_environmental(){
-	if grep -Eqi "/usr/local/telegram-desktop/bin" /etc/profile; then
-		source /etc/profile
-	else
-		echo 'export PATH=$PATH:/usr/local/telegram-desktop/bin' >> /etc/profile
-		source /etc/profile
-	fi
-}
-
 TG_VERSION=$(wget -q -O - https://github.com/telegramdesktop/tdesktop/tags | grep -v "beta" | grep -v "rc" | grep -m1 -E "telegramdesktop/tdesktop/releases/tag/v[0-9]+\.[0-9]+\.*[0-9]*" | sed -E "s/.*v([0-9]+\.[0-9]+\.*[0-9]*).*/\1/gm")
 
 cd /tmp
@@ -43,27 +34,17 @@ if [ -f tsetup.${TG_VERSION}.tar.xz ]; then
 	\rm -rf tsetup.${TG_VERSION}.tar.xz
 fi
 
-if [ -e /usr/local/telegram-desktop ]; then
-	\rm -rf /usr/local/telegram-desktop
+if [ -e /opt/Telegram ]; then
+	\rm -rf /opt/Telegram
 fi
-
-if [ -e /tmp/Telegram ]; then
-	\rm -rf /tmp/Telegram
-fi
-
-mkdir -p /usr/local/telegram-desktop/bin
 
 wget https://github.com/telegramdesktop/tdesktop/releases/download/v${TG_VERSION}/tsetup.${TG_VERSION}.tar.xz
 xz -d tsetup.${TG_VERSION}.tar.xz
-tar -xf tsetup.${TG_VERSION}.tar
-cd /tmp/Telegram
-mv Telegram Updater -t /usr/local/telegram-desktop/bin
-cd /tmp
-\rm -rf /tmp/Telegram tsetup.${TG_VERSION}.tar.xz tsetup.${TG_VERSION}.tar
+tar -xf tsetup.${TG_VERSION}.tar -C /opt
+\rm -rf tsetup.${TG_VERSION}.tar.xz tsetup.${TG_VERSION}.tar
 
 if [ -e ~/.local/share/TelegramDesktop ]; then
 	\rm -rf ~/.local/share/TelegramDesktop
 fi
 
-tg_environmental
 echo "The Telegram-Desktop ${TG_VERSION} install Success!"
