36a2fee1852f56096958f0c1c37b73d206f0800b
allen.w <koomox@163.com>
2019-03-20 14:49:09 +0800

    up

--- storage/linux/scripts/nginx/install_nginx1142.sh
+++ storage/linux/scripts/nginx/install_nginx1142.sh
@@ -140,13 +140,13 @@ chmod -R 755 ${NGINX_LETSENCRYPT}
 
 function create_system_user() {
 	USERNAME=$1
-	if ! `grep -Eq "^${USERNAME}" /etc/group` ; then
+	if [ `grep -Eq "^${USERNAME}" /etc/group` ]; then
 		groupadd -r ${USERNAME}
 	else
 		echo "${USERNAME} Group Already Exist!"
 	fi
 
-	if ! `grep -Eq "^${USERNAME}" /etc/passwd` ; then
+	if [ `grep -Eq "^${USERNAME}" /etc/passwd` ]; then
 		useradd -r -g ${USERNAME} -s /bin/false ${USERNAME}
 	else
 		echo "User ${USERNAME} Already Exist!"
