b733d2cf06c1363446387cc8164024b75ae4f796
allen.w <koomox@163.com>
2019-04-04 14:31:20 +0800

    up

--- storage/linux/scripts/shadowsocks/download_go_shadowsocks2.sh
+++ storage/linux/scripts/shadowsocks/download_go_shadowsocks2.sh
@@ -6,18 +6,9 @@ if [ -f go-shadowsocks2.tar.gz ]; then
 	\rm -rf go-shadowsocks2.tar.gz
 fi
 
-git clone https://github.com/shadowsocks/go-shadowsocks2.git --depth=1
+mkdir go-shadowsocks2
 cd go-shadowsocks2
-mkdir -p ./src/github.com/shadowsocks/go-shadowsocks2
-mv core ./src/github.com/shadowsocks/go-shadowsocks2
-mv socks ./src/github.com/shadowsocks/go-shadowsocks2
-mv shadowaead ./src/github.com/shadowsocks/go-shadowsocks2
-mv shadowstream ./src/github.com/shadowsocks/go-shadowsocks2
-mkdir -p ./src/github.com/aead/chacha20
-git clone https://github.com/aead/chacha20.git ./src/github.com/aead/chacha20 --depth=1
-mkdir -p ./src/golang.org/x/crypto
-git clone https://github.com/golang/crypto.git ./src/golang.org/x/crypto --depth=1
-find . -name .git | xargs rm -fr
+go get -u -d github.com/shadowsocks/go-shadowsocks2
 cd ..
 tar -zcvf go-shadowsocks2.tar.gz go-shadowsocks2
 ffsend upload go-shadowsocks2.tar.gz
--- storage/linux/scripts/shadowsocks/download_shadowsocks_go.sh
+++ storage/linux/scripts/shadowsocks/download_shadowsocks_go.sh
@@ -6,15 +6,9 @@ if [ -f shadowsocks-go.tar.gz ]; then
 	\rm -rf shadowsocks-go.tar.gz
 fi
 
-git clone https://github.com/shadowsocks/shadowsocks-go.git --depth=1
+mkdir shadowsocks-go
 cd shadowsocks-go
-mkdir -p ./src/github.com/shadowsocks/shadowsocks-go
-mv shadowsocks ./src/github.com/shadowsocks/shadowsocks-go
-mkdir -p ./src/github.com/aead/chacha20
-git clone https://github.com/aead/chacha20.git ./src/github.com/aead/chacha20 --depth=1
-mkdir -p ./src/golang.org/x/crypto
-git clone https://github.com/golang/crypto.git ./src/golang.org/x/crypto --depth=1
-find . -name .git | xargs rm -fr
+go get -u -d github.com/shadowsocks/shadowsocks-go
 cd ..
 tar -zcvf shadowsocks-go.tar.gz shadowsocks-go
 ffsend upload shadowsocks-go.tar.gz
