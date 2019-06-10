d88f434b04eb23c701d233953c9a2351d255ddc3
allen.w <koomox@163.com>
2019-06-10 19:03:07 +0800

    add

--- linux/nginx.md
+++ linux/nginx.md
@@ -12,3 +12,19 @@ curl -s https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scr
 chmod +x /tmp/download_nginx1142.sh
 /tmp/download_nginx1142.sh
 ```
+一键安装 Nginx 1.16.0 [查看源文件](../storage/linux/scripts/nginx/install_nginx1160.sh)         
+```sh
+wget https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nginx/install_nginx1160.sh
+chmod +x ./install_nginx1160.sh
+./install_nginx1160.sh
+```
+
+```sh
+sed -i 's/localhost/*.com www.*.com/g' /etc/nginx/conf.d/*.com.conf
+sed -i 's/$domain/*.com/g' /etc/nginx/conf.d/*.com.conf
+```
+```sh
+mkdir -p /etc/letsencrypt/live/$domain
+touch /etc/letsencrypt/live/$domain/fullchain.pem
+touch /etc/letsencrypt/live/$domain/privkey.pem
+```
---
+++ storage/linux/scripts/nginx/install_nginx1160.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+if [ ! -d make_nginx ]; then
+	\rm -rf make_nginx
+fi
+mkdir -p make_nginx && cd make_nginx
+
+wget https://nginx.org/download/nginx-1.16.0.tar.gz
+wget https://www.openssl.org/source/openssl-1.1.1c.tar.gz
+wget https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz
+wget https://www.zlib.net/zlib-1.2.11.tar.gz
+
+tar -zxf openssl-1.1.1c.tar.gz
+tar -zxf pcre-8.43.tar.gz
+tar -zxf zlib-1.2.11.tar.gz
+tar -zxf nginx-1.16.0.tar.gz
+
+cd nginx-1.16.0
+./configure --prefix=/usr/local/nginx \
+--conf-path=/etc/nginx/nginx.conf \
+--pid-path=/var/lib/nginx/nginx.pid \
+--lock-path=/var/lib/nginx/nginx.lock \
+--with-http_ssl_module \
+--with-http_v2_module \
+--with-http_sub_module \
+--with-http_gzip_static_module \
+--with-http_slice_module \
+--with-http_stub_status_module \
+--with-stream \
+--with-stream_ssl_module \
+--with-pcre=../pcre-8.43 \
+--with-zlib=../zlib-1.2.11 \
+--with-openssl=../openssl-1.1.1c
+make
+make install
+
+echo 'export PATH=$PATH:/usr/local/nginx/sbin' >> /etc/profile
+source /etc/profile
+
+mkdir -p /etc/nginx/conf.d /var/www/html /var/log/nginx /var/lib/nginx /var/www/letsencrypt
+
+wget -O /etc/nginx/conf.d/fastcgi_php.conf https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nginx/1.16.0/fastcgi_php.conf
+wget -O /etc/nginx/conf.d/letsencrypt.conf https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nginx/1.16.0/letsencrypt.conf
+wget -O /etc/nginx/conf.d/nginx-http-php.conf https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nginx/1.14.0/nginx-http-php.conf
+wget -O /etc/nginx/conf.d/nginx-ssl-fpm.conf https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nginx/1.16.0/nginx-ssl-fpm.conf
+wget -O /etc/nginx/conf.d/phpmyadmin.conf https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nginx/1.16.0/phpmyadmin.conf
+wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nginx/1.16.0/nginx-default.conf
+
+wget -O /etc/systemd/system/nginx.service https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nginx/1.14.0/nginx.service
+
+groupadd php-fpm
+useradd -r -g php-fpm -s /bin/false php-fpm
+
+groupadd nginx
+useradd -r -g nginx -s /bin/false nginx
+
+groupadd www-data
+useradd -r -g www-data -s /bin/false www-data
+
+usermod -a -G www-data php-fpm
+usermod -a -G www-data nginx
+
+chmod -R 755 /etc/nginx/conf.d /var/www/html /var/log/nginx /var/lib/nginx /var/www/letsencrypt
+chown -R www-data:www-data /var/www/html /var/www/letsencrypt
+
+systemctl enable nginx
+systemctl start nginx
+systemctl status nginx
+
+nginx -v
