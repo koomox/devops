2fb470aaf5d871525ce26c01e3239dc350f38492
koomox <koomoxs@gmail.com>
2020-04-19 14:31:57 +0800

    fix

---
+++ storage/linux/scripts/browser.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+FIREFOX_VERSION=75.0
+FIREFOX_DEVELOPER_EDITION_VERSION=76.0b5
+TOR_VERSION=9.0.9
+
+wget -O en_ChromeStandaloneSetup64.exe https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B754CC110-B9C8-798B-4231-9054058921FC%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe
+wget -O en_ChromeStandaloneSetup.exe https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B754CC110-B9C8-798B-4231-9054058921FC%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx86-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup.exe
+wget -O zhCN_ChromeStandaloneSetup64.exe https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B754CC110-B9C8-798B-4231-9054058921FC%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe
+wget -O zhCN_ChromeStandaloneSetup.exe https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B754CC110-B9C8-798B-4231-9054058921FC%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx86-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup.exe
+
+wget -O en_Firefox_Developer_Edition_x64_${FIREFOX_DEVELOPER_EDITION_VERSION}.exe https://download-installer.cdn.mozilla.net/pub/devedition/releases/${FIREFOX_DEVELOPER_EDITION_VERSION}/win64/en-US/Firefox%20Setup%20${FIREFOX_DEVELOPER_EDITION_VERSION}.exe
+wget -O en_Firefox_Developer_Edition_x86_${FIREFOX_DEVELOPER_EDITION_VERSION}.exe https://download-installer.cdn.mozilla.net/pub/devedition/releases/${FIREFOX_DEVELOPER_EDITION_VERSION}/win32/en-US/Firefox%20Setup%20${FIREFOX_DEVELOPER_EDITION_VERSION}.exe
+wget -O zhCN_Firefox_Developer_Edition_x64_${FIREFOX_DEVELOPER_EDITION_VERSION}.exe https://download-installer.cdn.mozilla.net/pub/devedition/releases/${FIREFOX_DEVELOPER_EDITION_VERSION}/win64/zh-CN/Firefox%20Setup%20${FIREFOX_DEVELOPER_EDITION_VERSION}.exe
+wget -O zhCN_Firefox_Developer_Edition_x86_${FIREFOX_DEVELOPER_EDITION_VERSION}.exe https://download-installer.cdn.mozilla.net/pub/devedition/releases/${FIREFOX_DEVELOPER_EDITION_VERSION}/win32/zh-CN/Firefox%20Setup%20${FIREFOX_DEVELOPER_EDITION_VERSION}.exe
+
+wget -O en_Firefox_x64_${FIREFOX_VERSION}.exe https://download-installer.cdn.mozilla.net/pub/firefox/releases/${FIREFOX_VERSION}/win64/en-US/Firefox%20Setup%20${FIREFOX_VERSION}.exe
+wget -O en_Firefox_x86_${FIREFOX_VERSION}.exe https://download-installer.cdn.mozilla.net/pub/firefox/releases/${FIREFOX_VERSION}/win32/en-US/Firefox%20Setup%20${FIREFOX_VERSION}.exe
+wget -O zhCN_Firefox_x64_${FIREFOX_VERSION}.exe https://download-installer.cdn.mozilla.net/pub/firefox/releases/${FIREFOX_VERSION}/win64/zh-CN/Firefox%20Setup%20${FIREFOX_VERSION}.exe
+wget -O zhCN_Firefox_x86_${FIREFOX_VERSION}.exe https://download-installer.cdn.mozilla.net/pub/firefox/releases/${FIREFOX_VERSION}/win32/zh-CN/Firefox%20Setup%20${FIREFOX_VERSION}.exe
+
+wget -O torbrowser-install-${TOR_VERSION}_en-US.exe https://www.torproject.org/dist/torbrowser/${TOR_VERSION}/torbrowser-install-${TOR_VERSION}_en-US.exe
+wget -O torbrowser-install-win64-${TOR_VERSION}_en-US.exe https://www.torproject.org/dist/torbrowser/${TOR_VERSION}/torbrowser-install-win64-${TOR_VERSION}_en-US.exe
+
+wget -O torbrowser-install-${TOR_VERSION}_zh-CN.exe https://www.torproject.org/dist/torbrowser/${TOR_VERSION}/torbrowser-install-${TOR_VERSION}_zh-CN.exe
+wget -O torbrowser-install-win64-${TOR_VERSION}_zh-CN.exe https://www.torproject.org/dist/torbrowser/${TOR_VERSION}/torbrowser-install-win64-${TOR_VERSION}_zh-CN.exe
