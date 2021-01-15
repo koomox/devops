e3acc5777bfe3fd30c08ed50670df6dfc3d70fce
koomox <koomoxs@gmail.com>
2021-01-15 22:32:32 +0800

    up

---
+++ storage/linux/scripts/nginx/1.18.0/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+cd /tmp
+wget -O nginx-1.18.0.tar.gz https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/nginx/1.18.0/binary/nginx-1.18.0.tar.gz
+tar -zxf nginx-1.18.0.tar.gz
+\rm -rf /usr/local/nginx /etc/nginx
+mv nginx-1.18.0 /usr/local/nginx
+cd /usr/local/nginx
+mv conf /etc/nginx
+cp -f nginx.service /etc/systemd/system/nginx.service
+
+echo 'export PATH=$PATH:/usr/local/nginx/sbin' >> /etc/profile
+source /etc/profile
+
+mkdir -p /var/www/html /var/log/nginx /var/lib/nginx /var/www/letsencrypt /web/public /web/phpMyAdmin
+
+groupadd php-fpm
+useradd -r -g php-fpm -s /bin/false php-fpm
+
+groupadd nginx
+useradd -r -g nginx -s /bin/false nginx
+
+groupadd www-data
+useradd -r -g www-data -s /bin/false www-data
+
+usermod -a -G www-data php-fpm
+usermod -a -G www-data nginx
+
+chmod -R 755 /etc/nginx/conf.d /var/www/html /var/log/nginx /var/lib/nginx /var/www/letsencrypt /web/public /web/phpMyAdmin
+chown -R www-data:www-data /var/www/html /var/www/letsencrypt /web/public /web/phpMyAdmin
+
+systemctl enable nginx
+systemctl start nginx
+systemctl status nginx
+
+nginx -v
