1a304a98d40beb0930bfb9700d733362535ccbfd
allen.w <koomoxs@gmail.com>
2022-12-17 09:04:25 +0800

    commit

--- storage/linux/scripts/teamviewer.sh
+++ storage/linux/scripts/teamviewer.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-\rm -rf /opt/teamviewer
-mkdir -p /opt/teamviewer
-cd /opt/teamviewer
+DATETIME=$(date +%Y%m%d)
+\rm -rf ./teamviewer
+mkdir -p ./teamviewer
+cd ./teamviewer
 
-wget https://download.teamviewer.com/download/TeamViewerQS.exe
-wget https://download.teamviewer.com/download/TeamViewer_Host_Setup.exe
-wget https://download.teamviewer.com/download/TeamViewerPortable.zip
+wget -O TeamViewerQS_${DATETIME}.exe https://download.teamviewer.com/download/TeamViewerQS.exe
+wget -O TeamViewer_Host_Setup_${DATETIME}.exe https://download.teamviewer.com/download/TeamViewer_Host_Setup.exe
+wget -O TeamViewerPortable_${DATETIME}.zip https://download.teamviewer.com/download/TeamViewerPortable.zip
 
-wget https://download.teamviewer.com/download/TeamViewerQS.dmg
-wget https://download.teamviewer.com/download/TeamViewerHost.dmg
-wget https://download.teamviewer.com/download/TeamViewer.dmg
+wget -O TeamViewerQS_${DATETIME}.dmg https://download.teamviewer.com/download/TeamViewerQS.dmg
+wget -O TeamViewerHost_${DATETIME}.dmg https://download.teamviewer.com/download/TeamViewerHost.dmg
+wget -O TeamViewer_${DATETIME}.dmg https://download.teamviewer.com/download/TeamViewer.dmg
 
-wget https://download.teamviewer.com/download/TeamViewerQS.apk
-wget https://download.teamviewer.com/download/TeamViewerHost.apk
-wget https://download.teamviewer.com/download/TeamViewer.apk
+wget -O teamviewer_qs_${DATETIME}.tar.gz https://download.teamviewer.com/download/teamviewer_qs.tar.gz
+wget -O teamviewer-host_amd64_${DATETIME}.deb https://download.teamviewer.com/download/linux/teamviewer-host_amd64.deb
+wget -O teamviewer_amd64_${DATETIME}.deb https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 
-tar -czvf TeamViewer.tar.gz ./*
+tar -czvf TeamViewer_${DATETIME}.tar.gz ./*
--- storage/linux/scripts/xss/x-serv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget -O /etc/systemd/system/x-serv.service https://raw.githubusercontent.com/koomox/devops/master/storage/linux/scripts/xss/x-serv.service
-
-systemctl enable x-serv.service
-systemctl start x-serv.service
-systemctl status x-serv.service
