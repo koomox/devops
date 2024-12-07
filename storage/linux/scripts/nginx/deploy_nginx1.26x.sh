70011711c2a147418a7966546c1320b1adc92111
allen.w <koomoxs@gmail.com>
2024-12-07 13:03:53 +0800

    commit

--- storage/linux/scripts/nginx/deploy_nginx1.26x.sh
+++ storage/linux/scripts/nginx/deploy_nginx1.26x.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NGINX_VERSION=NGINX=$(wget -qO- --no-check-certificate https://nginx.org/en/download.html | grep -m1 -E "nginx-1.26.([0-9]+).tar.gz" | sed -E "s/.*(nginx-1.26.[0-9]+).tar.gz.*/\1/gm" )
+NGINX_VERSION=$(wget -qO- --no-check-certificate https://nginx.org/en/download.html | grep -m1 -E "nginx-1.26.([0-9]+).tar.gz" | sed -E "s/.*(nginx-1.26.[0-9]+).tar.gz.*/\1/gm" )
 OPENSSL_VERSION=$(wget -qO- --no-check-certificate https://www.openssl-library.org/source/ | grep -m1 -E "openssl-3(\.[0-9]+){0,2}.tar.gz" | sed -E "s/.*(openssl-.*).tar.gz.*/\1/gm" )
 PCRE2_VERSION=pcre2-10.44
 ZLIB_VERSION=$(wget -qO- --no-check-certificate https://zlib.net/ | grep -m1 -E "zlib-([0-9]+\.){0,3}tar.gz" | sed -E "s/.*(zlib-.*).tar.gz.*/\1/gm" )
